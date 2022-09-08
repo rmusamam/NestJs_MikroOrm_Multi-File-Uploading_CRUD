ec500669aa1ac7eff0938be8884b03d8ab0ac89f
Muhammad Usama <usama.mehmood@techoverflow.io>
2022-09-08 23:19:58 +0500

    uploading multiple files

--- folder-attachment/folder-attachment.controller.ts
+++ folder-attachment/folder-attachment.controller.ts
@@ -1,13 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { FolderAttachmentService } from './folder-attachment.service';
 import { CreateFolderAttachmentDto } from './dto/create-folder-attachment.dto';
 import { UpdateFolderAttachmentDto } from './dto/update-folder-attachment.dto';
+
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from "path";
 
 @Controller('folder-attachment')
 export class FolderAttachmentController {
   constructor(private readonly folderAttachmentService: FolderAttachmentService) {}
 
   @Post()
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'avatar', },
+  
+  ],{storage: diskStorage({
+    destination: './assets/folderAttachment',
+    filename: (req, file, callBack) => {
+      const fileName = path.parse(file.originalname).name.replace(/\s/g, '') + Date.now();
+      const extension = path.parse(file.originalname).ext;
+      callBack(null, `${fileName}${extension}`)
+    }
+  }),}))
+  uploadFile(@UploadedFiles() files: { avatar?: Express.Multer.File[], background?: Express.Multer.File[] }) {
+    console.log('hello files',files);
+  }
+  
   create(@Body() createFolderAttachmentDto: CreateFolderAttachmentDto) {
     return this.folderAttachmentService.create(createFolderAttachmentDto);
   }
